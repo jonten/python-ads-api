971dd2aa5bd4feff13f699c506e9f68e1727acbb
Jon Norman <jonte.n@gmail.com>
2020-11-17 23:27:22 +0100

    Added pylint options, docstrings and removed global variables

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+# pylint: disable=f-string-without-interpolation
+
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 import asyncpg
 
-
-conn = None
-row = None
-
 class Ad(BaseModel):
+    """Create Ad class with a required subject, body, email and a optional price"""
     subject: str
     body: str
     email: EmailStr
@@ -15,24 +16,28 @@ class Ad(BaseModel):
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", status_code=200)
 async def root():
+    """Default route with info message"""
     return {"message": "Default API endpoint is /ads and documentation endpoint is /docs"}
 
 @app.post("/ads/", status_code=201)
-async def create_ad(ad: Ad):
-    await db_create_ad(ad)
-    return ad
+async def create_ad(new_ad: Ad):
+    """Route function for creating new ads"""
+    await db_create_ad(new_ad)
+    return new_ad
 
 
-async def db_create_ad(ad):
+async def db_create_ad(new_ad):
+    """Function for creating new ads in the database"""
     conn = await asyncpg.connect(user="adsuser", database="adsdb")
     query = "INSERT INTO ads  (subject, body, email, price) VALUES ($1, $2, $3, $4)"
-    await conn.execute(query, ad.subject, ad.body, ad.email, ad.price)
+    await conn.execute(query, new_ad.subject, new_ad.body, new_ad.email, new_ad.price)
 
 
 @app.on_event("startup")
 async def startup_event():
+    """Startup function for setting up the database/connection"""
     try:
         conn = await asyncpg.connect('postgresql://localhost/adsdb')
     except asyncpg.InvalidCatalogNameError:
@@ -68,6 +73,8 @@ async def startup_event():
 
 @app.on_event("shutdown")
 def shutdown_event():
+    """Shutdown function for closing the database connection"""
+    conn = None
     conn.close()
     with open("log.txt", mode="a") as log:
         log.write("Application shutdown")
