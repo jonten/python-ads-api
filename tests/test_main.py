6009bb27486eae3d01d3ac5a3c8e7bd21bedc51e
Jon Norman <jonte.n@gmail.com>
2020-11-30 00:59:26 +0100

    WIP: Adding more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from app.main import app, Ad, db_create_ad
+from app.main import app, Ad, db_create_ad, db_delete_ad
 
 client = TestClient(app)
 
@@ -25,5 +25,16 @@ def test_create_ad():
     }
 
 async def test_db_create_ad():
-    ad = Ad(subject="Rubrik", body="bla", email="test.testsson@yahoo.com", price="345.50")
+    ad = Ad(subject="Snöskoter", body="Fin Lynx snöskoter säljes till högstbjudande", email="test.testsson@yahoo.com", price="5600.50")
     await db_create_ad(ad)
+
+def test_delete_ad():
+    response = client.delete(
+        "/ads",
+        json={"ad_id": 3},
+    )
+    assert response.status_code == 200
+
+async def test_db_delete_ad():
+    ad_id = 3
+    await db_delete_ad(ad_id)
